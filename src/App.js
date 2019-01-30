95638f37c2399187630d7a90af0dd2165b6ecf6c
Jane Lee <Yesdoing13@gmail.com>
2019-01-30 22:59:36 +0900

    theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes, ThemeProvider } from "styled-components";
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -8,38 +9,44 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.8);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: pink;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  border-radius: 5px;
-  ${awesomeCard}
+const Card = styled.div`
+  background-color: red;
 `;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+
 
 class App extends Component {
   render() {
     return (
+      <ThemeProvider theme={theme}>
       <Fragment>
         <Container>
-          <Input placeholder="Hello"/>
+          <Form />
         </Container>
         <GlobalStyle />
       </Fragment>
+      </ThemeProvider>
     );
   }
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
+
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+    mainColor: "#bdc3c7",
+    dangerColor: "#c0392b",
+    successColor: "#2ecc71",
+};
+
+export default theme;
